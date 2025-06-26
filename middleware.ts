9cc2bd12320d810a2ad396f3b4d49d1df33e4a04
ftmhabt <ftmh.a78@gmail.com>
2025-06-26 20:56:17 +0330

    Add middleware

---
+++ middleware.ts
@@ -0,0 +1,42 @@
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+const PUBLIC_FILE = /\.(.*)$/;
+
+export const validateJWT = async (jwt: string) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+  return payload;
+};
+
+export default async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/static") ||
+    pathname.startsWith("/signin") ||
+    pathname.startsWith("/register") ||
+    PUBLIC_FILE.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
+  const jwt = req.cookies.get(process.env.COOKIE_NAME || "");
+
+  if (!jwt) {
+    req.nextUrl.pathname = "/signin";
+    return NextResponse.redirect(req.nextUrl);
+  }
+
+  try {
+    await validateJWT(jwt.value);
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+    req.nextUrl.pathname = "/signin";
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
