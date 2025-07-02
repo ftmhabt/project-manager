557a7305e70a474e5592bd57fa5acc603b84a581
ftmhabt <ftmh.a78@gmail.com>
2025-07-02 21:33:10 +0330

    Add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Project Manager App",
+  description: "A modern project manager web app built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
