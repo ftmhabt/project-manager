f23f8aa4f69b2c1b1a3b3e7ccf95724e206b0250
ftmhabt <ftmh.a78@gmail.com>
2025-07-19 14:00:07 +0330

    Show due date

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -8,6 +8,7 @@ import IsChecked from "./IsChecked";
 import DeleteButton from "./DeleteButton";
 import EditTask from "./EditTask";
 import GlassPane from "./GlassPane";
+import { calculateDue, timeLeftUntil } from "@/lib/due";
 
 const NUMBERS: number = 10;
 const getData = async () => {
@@ -61,15 +62,18 @@ const TaskCard = async ({
           tasks.map((task) => (
             <div
               key={task.id}
-              className="flex justify-between md:flex-row flex-col gap-2"
+              className="flex justify-between md:flex-row flex-col gap-2  w-full"
             >
               <div className="py-2">
                 <div>
                   <span className="text-2xl text-gray-700 ">{task.name}</span>
                 </div>
-                <div>
+                <div className="grid grid-cols-2">
                   <span className="text-sm text-gray-500">
                     {task.description}
+                  </span>
+                  <span className="text-sm text-gray-500">
+                    {task.due && timeLeftUntil(task.due)}
                   </span>
                 </div>
               </div>
