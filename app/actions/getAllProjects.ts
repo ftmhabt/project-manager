3c45fc293aaf0a76efd0ce9c0dd2a82859dc465a
ftmhabt <ftmh.a78@gmail.com>
2025-08-04 00:41:13 +0330

    Get user's team's projects too in edit task form

--- app/actions/getAllProjects.ts
+++ app/actions/getAllProjects.ts
@@ -1,9 +1,24 @@
 "use server";
 
+import { getAuthenticatedUser } from "@/lib/auth";
 import { db } from "@/lib/db";
 
 export const getAllProjects = async () => {
+  const user = await getAuthenticatedUser();
+
   const projects = await db.project.findMany({
+    where: {
+      OR: [
+        { ownerId: user?.id }, // Projects the user owns
+        {
+          team: {
+            members: {
+              some: { userId: user?.id }, // Projects from teams where user is a member
+            },
+          },
+        },
+      ],
+    },
     select: {
       id: true,
       name: true,
