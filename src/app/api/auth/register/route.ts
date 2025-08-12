e3630c2b4b346b754263691d0da77d75ef394c3e
ftmhabt <ftmh.a78@gmail.com>
2025-08-12 23:37:10 +0330

    Move api routes from pages/api to app directory

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,43 @@
+import { createJWT, hashPassword } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { serialize } from "cookie";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, password, firstName, lastName } = await req.json();
+
+  try {
+    const user = await db.user.create({
+      data: {
+        email,
+        password: await hashPassword(password),
+        firstName,
+        lastName,
+      },
+    });
+
+    const jwt = await createJWT(user);
+
+    const cookie = serialize(process.env.COOKIE_NAME as string, jwt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+      sameSite: "lax",
+    });
+
+    return new NextResponse(JSON.stringify({}), {
+      status: 201,
+      headers: {
+        "Set-Cookie": cookie,
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Register error:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,61 @@
+import { comparePasswords, createJWT } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { serialize } from "cookie";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json();
+
+  if (
+    !email ||
+    typeof email !== "string" ||
+    !password ||
+    typeof password !== "string"
+  ) {
+    return NextResponse.json(
+      { message: "Email and password are required and must be strings" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const isValid = await comparePasswords(password, user.password);
+
+    if (!isValid) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 401 }
+      );
+    }
+
+    const jwt = await createJWT(user);
+
+    const cookie = serialize(process.env.COOKIE_NAME as string, jwt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+      sameSite: "lax",
+    });
+
+    return new NextResponse(
+      JSON.stringify({ message: "Signed in successfully" }),
+      {
+        status: 200,
+        headers: { "Set-Cookie": cookie, "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    console.error("Signin error:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
